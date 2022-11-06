0aeac79393f87f2155f11f8da2dc4a15b880c9fe
Arnout Van Nieuwenhove <arnout@sowiso.nl>
2022-11-06 21:04:25 +0100

    Refactor updateQuality

--- src/gilded-tros.ts
+++ src/gilded-tros.ts
@@ -1,65 +1,76 @@
 import {Item} from './item';
+import It = jest.It;
+
+const ItemTypesEnum = {
+    WINE: "Good Wine",
+    PASS_RE_FACTOR: "Backstage passes for Re:Factor",
+    PASS_HAXX: "Backstage passes for HAXX",
+    BDAWG: "B-DAWG Keychain"
+}
+
+function updateRegular(item: Item) {
+    if (item.quality > 0) {
+        item.quality = item.quality - 1;
+    }
+
+    item.sellIn = item.sellIn - 1;
+
+    if (item.sellIn < 0 && item.quality > 0) {
+        item.quality = item.quality - 1;
+    }
+}
+
+function updatePass(item: Item) {
+    if (item.quality < 50) {
+        item.quality = item.quality + 1;
+
+        if (item.sellIn < 11) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        }
+
+        if (item.sellIn < 6) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
+            }
+        }
+    }
+
+    item.sellIn = item.sellIn - 1;
+
+    if (item.sellIn < 0) {
+        item.quality = 0;
+    }
+}
+
+function updateGoodWine(item: Item) {
+    if (item.quality < 50) {
+        item.quality = item.quality + 1;
+    }
+
+    item.sellIn = item.sellIn - 1;
+
+    if (item.sellIn < 0) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+    }
+}
 
 export class GildedTros {
-
-    constructor(public items: Array<Item>) {
-
-    }
+    constructor(public items: Array<Item>) {}
 
     public updateQuality(): void {
         this.items.forEach((item: Item) => {
-            if (item.name != 'Good Wine' && item.name != 'Backstage passes for Re:Factor'
-                && item.name != 'Backstage passes for HAXX') {
-                if (item.quality > 0) {
-                    if (item.name != 'B-DAWG Keychain') {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    //another bug, should also contain 'Backstage passes for HAXX'
-                    if (item.name == 'Backstage passes for Re:Factor' || item.name == 'Backstage passes for HAXX') {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (item.name != 'B-DAWG Keychain') {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (item.name != 'Good Wine') {
-                    //dit moet een && ipv || zijn?
-                    if (item.name != 'Backstage passes for Re:Factor' && item.name != 'Backstage passes for HAXX') {
-                        if (item.quality > 0) {
-                            if (item.name != 'B-DAWG Keychain') {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
+            if (item.name === ItemTypesEnum.WINE)
+                updateGoodWine(item);
+            else if (item.name === ItemTypesEnum.BDAWG)
+                return;
+            else if (item.name === ItemTypesEnum.PASS_HAXX || item.name === ItemTypesEnum.PASS_RE_FACTOR)
+                updatePass(item);
+            else
+                updateRegular(item);
         });
     }
-
 }
-
